bf782b1bf36fbf14511c911c818691c134d9e8b7
Natanael Tan <nonnoxer@gmail.com>
2021-05-17 13:58:02 +0800

    improvements to paragraph detection

--- counter.py
+++ counter.py
@@ -6,12 +6,13 @@ def count_words(filename, regex):
     words = 0
 
     for para in doc.paragraphs:
-        if para.paragraph_format.first_line_indent is not None and para.paragraph_format.first_line_indent > 0:
-            text = para.text
-            cleantext = ''
-            itcs = [(i.span()) for i in re.finditer(regex, text)]
-            for i in itcs:
-                words -= len(para.text[i[0]:i[1]].split(' '))
-            words += len(para.text.split(' '))
+        if para.style.name == 'Normal (Web)':
+            if not (para.paragraph_format.first_line_indent is not None and para.paragraph_format.first_line_indent < 0):
+                text = para.text
+                cleantext = ''
+                itcs = [(i.span()) for i in re.finditer(regex, text)]
+                for i in itcs:
+                    words -= len(para.text[i[0]:i[1]].split(' '))
+                words += len(para.text.split(' '))
 
     return words
