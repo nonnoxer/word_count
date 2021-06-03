7c24a08c6770940182aea48da705aa94ef442465
Natanael Tan <nonnoxer@gmail.com>
2021-06-03 21:57:13 +0800

    avoid counting blank lines

--- counter.py
+++ counter.py
@@ -6,14 +6,14 @@ def count_words(filename, regex):
     words = 0
 
     for para in doc.paragraphs:
-        print(para.style.name)
         if para.style.name.find('Normal') != -1:
             if not (para.paragraph_format.first_line_indent is not None and para.paragraph_format.first_line_indent < 0):
-                text = para.text
-                cleantext = ''
+                text = para.text.strip()
+                if text == '':
+                    continue
                 itcs = [(i.span()) for i in re.finditer(regex, text)]
                 for i in itcs:
-                    words -= len(para.text[i[0]:i[1]].split(' '))
-                words += len(para.text.split(' '))
+                    words -= len(text[i[0]:i[1]].split(' '))
+                words += len(text.split(' '))
 
     return words
